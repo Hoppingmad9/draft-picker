a29d0a1556ef97460c73c51835c54610c9845405
Sam Dean <samdena09@gmail.com>
2021-03-15 14:46:15 +0000

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,324 @@
+var champs = ["./images/AatroxSquare.png",
+	"./images/AhriSquare.png",
+	"./images/AkaliSquare.png",
+	"./images/AlistarSquare.png",
+	"./images/AmumuSquare.png",
+	"./images/AniviaSquare.png",
+	"./images/AnnieSquare.png",
+	"./images/ApheliosSquare.png",
+	"./images/AsheSquare.png",
+	"./images/Aurelion_SolSquare.png",
+	"./images/AzirSquare.png",
+	"./images/BardSquare.png",
+	"./images/BlitzcrankSquare.png",
+	"./images/BrandSquare.png",
+	"./images/BraumSquare.png",
+	"./images/CaitlynSquare.png",
+	"./images/CamilleSquare.png",
+	"./images/CassiopeiaSquare.png",
+	"./images/Cho%27GathSquare.png",
+	"./images/CorkiSquare.png",
+	"./images/DariusSquare.png",
+	"./images/DianaSquare.png",
+	"./images/Dr._MundoSquare.png",
+	"./images/DravenSquare.png",
+	"./images/EkkoSquare.png",
+	"./images/EliseSquare.png",
+	"./images/EvelynnSquare.png",
+	"./images/EzrealSquare.png",
+	"./images/FiddlesticksSquare.png",
+	"./images/FioraSquare.png",
+	"./images/FizzSquare.png",
+	"./images/GalioSquare.png",
+	"./images/GangplankSquare.png",
+	"./images/GarenSquare.png",
+	"./images/GnarSquare.png",
+	"./images/GragasSquare.png",
+	"./images/GravesSquare.png",
+	"./images/HecarimSquare.png",
+	"./images/HeimerdingerSquare.png",
+	"./images/IllaoiSquare.png",
+	"./images/IreliaSquare.png",
+	"./images/IvernSquare.png",
+	"./images/JannaSquare.png",
+	"./images/Jarvan_IVSquare.png",
+	"./images/JaxSquare.png",
+	"./images/JayceSquare.png",
+	"./images/JhinSquare.png",
+	"./images/JinxSquare.png",
+	"./images/Kai%27SaSquare.png",
+	"./images/KalistaSquare.png",
+	"./images/KarmaSquare.png",
+	"./images/KarthusSquare.png",
+	"./images/KassadinSquare.png",
+	"./images/KatarinaSquare.png",
+	"./images/KayleSquare.png",
+	"./images/KaynSquare.png",
+	"./images/KennenSquare.png",
+	"./images/Kha%27ZixSquare.png",
+	"./images/KindredSquare.png",
+	"./images/KledSquare.png",
+	"./images/Kog%27MawSquare.png",
+	"./images/LeBlancSquare.png",
+	"./images/Lee_SinSquare.png",
+	"./images/LeonaSquare.png",
+	"./images/LilliaSquare.png",
+	"./images/LissandraSquare.png",
+	"./images/LucianSquare.png",
+	"./images/LuluSquare.png",
+	"./images/LuxSquare.png",
+	"./images/MalphiteSquare.png",
+	"./images/MalzaharSquare.png",
+	"./images/MaokaiSquare.png",
+	"./images/Master_YiSquare.png",
+	"./images/MissFortuneSquare.png",
+	"./images/MordekaiserSquare.png",
+	"./images/MorganaSquare.png",
+	"./images/NamiSquare.png",
+	"./images/NasusSquare.png",
+	"./images/NautilusSquare.png",
+	"./images/NeekoSquare.png",
+	"./images/NidaleeSquare.png",
+	"./images/NocturneSquare.png",
+	"./images/Nunu_%26_WillumpSquare.png",
+	"./images/OlafSquare.png",
+	"./images/OriannaSquare.png",
+	"./images/OrnnSquare.png",
+	"./images/PantheonSquare.png",
+	"./images/PoppySquare.png",
+	"./images/PykeSquare.png",
+	"./images/QiyanaSquare.png",
+	"./images/QuinnSquare.png",
+	"./images/RakanSquare.png",
+	"./images/RammusSquare.png",
+	"./images/Rek%27SaiSquare.png",
+	"./images/RellSquare.png",
+	"./images/RenektonSquare.png",
+	"./images/RengarSquare.png",
+	"./images/RivenSquare.png",
+	"./images/RumbleSquare.png",
+	"./images/RyzeSquare.png",
+	"./images/SamiraSquare.png",
+	"./images/SejuaniSquare.png",
+	"./images/SennaSquare.png",
+	"./images/SeraphineSquare.png",
+	"./images/SettSquare.png",
+	"./images/ShacoSquare.png",
+	"./images/ShenSquare.png",
+	"./images/ShyvanaSquare.png",
+	"./images/SingedSquare.png",
+	"./images/SionSquare.png",
+	"./images/SivirSquare.png",
+	"./images/SkarnerSquare.png",
+	"./images/SonaSquare.png",
+	"./images/SorakaSquare.png",
+	"./images/SwainSquare.png",
+	"./images/SylasSquare.png",
+	"./images/SyndraSquare.png",
+	"./images/Tahm_KenchSquare.png",
+	"./images/TaliyahSquare.png",
+	"./images/TalonSquare.png",
+	"./images/TaricSquare.png",
+	"./images/TeemoSquare.png",
+	"./images/ThreshSquare.png",
+	"./images/TristanaSquare.png",
+	"./images/TrundleSquare.png",
+	"./images/TryndamereSquare.png",
+	"./images/Twisted_FateSquare.png",
+	"./images/TwitchSquare.png",
+	"./images/UdyrSquare.png",
+	"./images/UrgotSquare.png",
+	"./images/VarusSquare.png",
+	"./images/VayneSquare.png",
+	"./images/VeigarSquare.png",
+	"./images/Vel%27KozSquare.png",
+	"./images/ViegoSquare.png",
+	"./images/ViktorSquare.png",
+	"./images/ViSquare.png",
+	"./images/VladimirSquare.png",
+	"./images/VolibearSquare.png",
+	"./images/WarwickSquare.png",
+	"./images/WukongSquare.png",
+	"./images/XayahSquare.png",
+	"./images/XerathSquare.png",
+	"./images/Xin_ZhaoSquare.png",
+	"./images/YasuoSquare.png",
+	"./images/YoneSquare.png",
+	"./images/YorickSquare.png",
+	"./images/YuumiSquare.png",
+	"./images/ZacSquare.png",
+	"./images/ZedSquare.png",
+	"./images/ZiggsSquare.png",
+	"./images/ZileanSquare.png",
+	"./images/ZoeSquare.png",
+	"./images/ZyraSquare.png"
+];
+
+var imgs = [];
+
+var numPlayers;
+var players = [];
+var playerTurn = 0;
+
+var picks = 10;
+var picked = [];
+var totalPicks;
+
+var playerRows = 4;
+var playerCols = 2;
+var playerPickRows = 2;
+var playerPickCols = 5;
+let thumbSize;
+
+function preload() {
+	champs.forEach(element => imgs.push(loadImage(element)));
+}
+
+function setup() {
+	let minPlayers = 2;
+	let maxPlayers = 21;
+	numPlayers = prompt('How many players do you have? Min: '+minPlayers+'. Max: '+maxPlayers+' for nice formatting. In theory unlimited.');
+	for (let i = 0; i < numPlayers; i++) {
+		let playerNum = i+1;
+		players.push([prompt('Name of player '+playerNum+'.'),0]);
+	}
+	let minPicks = 1;
+	let maxPicks;
+	do {
+		if (numPlayers > 21) {
+			maxPicks = 3;
+			playerRows = Math.ceil(numPlayers/4);
+			playerCols = 4;
+			playerPickRows = 1;
+			playerPickCols = 3;
+		} else if (numPlayers > 14) {
+			maxPicks = 3;
+			playerRows = 7;
+			playerCols = 3;
+			playerPickRows = 1;
+			playerPickCols = 3;
+		} else if (numPlayers > 12) {
+			maxPicks = 5;
+			playerRows = 7;
+			playerCols = 2;
+			playerPickRows = 1;
+			playerPickCols = 5;
+		} else if (numPlayers > 8) {
+			maxPicks = 6;
+			playerRows = 4;
+			playerCols = 3;
+			playerPickRows = 2;
+			playerPickCols = 3;
+		} else {
+			maxPicks = 10;
+			playerRows = 4;
+			playerCols = 2;
+			playerPickRows = 2;
+			playerPickCols = 5;
+		}
+		maxPicks = Math.min(Math.floor(154/numPlayers),maxPicks);
+		picks = prompt('How many picks each? Min: '+minPicks+'. Max: '+maxPicks+'.')
+		picks = Math.min(maxPicks,picks);
+		picks = Math.max(minPicks,picks);
+		console.log(picks);
+		console.log(Number.isInteger(picks));
+	} while (!Number.isInteger(picks));
+	createCanvas(windowWidth, windowHeight);
+	totalPicks = picks*numPlayers;
+
+	if (numPlayers > 21) {
+		thumbSize = Math.min(width*0.4/11,height/(playerRows*2));
+	} else {
+		thumbSize = Math.min(width*0.4/11,height/14);
+	}
+}
+
+function draw() {
+	background(50);
+	for (let i = 0; i < numPlayers; i++) {
+		players[i][1] = 0;
+	}
+	for (let i = 0; i < 14; i++){
+		for (let j = 0; j < 11; j++) {
+			image(imgs[i*11+j],j*thumbSize,i*thumbSize,thumbSize,thumbSize);
+			if (picked[i*11+j] >= 0) {
+				fill(50,50,50,220);
+				rect(j*thumbSize,i*thumbSize,thumbSize,thumbSize);
+				push();
+				if (numPlayers > 21) {
+					translate(0.45*width,0);
+				} else {
+					translate(0.6*width,0);
+				}
+				let playerPick = picked[i*11+j];
+				let playerPickRow = Math.floor(players[playerPick][1]/playerPickCols);
+				let playerPickCol = Math.floor(players[playerPick][1]%playerPickCols);
+
+				let playerHeight = height/playerRows;
+				let playerWidth;
+				if (playerCols == 2) {
+					playerWidth = thumbSize*5;
+				} else {
+					playerWidth = thumbSize*3;
+				}
+				let playerX = (playerPick%playerCols)*(playerWidth+thumbSize);
+				let playerY = Math.floor(playerPick/playerCols)*playerHeight+thumbSize;
+				image(imgs[i*11+j],playerX+playerPickCol*thumbSize,playerY+playerPickRow*thumbSize,thumbSize,thumbSize);
+				pop();
+				players[playerPick][1]++;
+			}
+		}
+	}
+	push();
+	if (numPlayers > 21) {
+		translate(0.45*width,0);
+	} else {
+		translate(0.6*width,0);
+	}
+	let playerHeight = height/playerRows;
+	let playerWidth;
+	if (playerCols == 2) {
+		playerWidth = thumbSize*5;
+	} else {
+		playerWidth = thumbSize*3;
+	}
+	for (let i = 0; i < playerRows; i++) {
+		for (let j = 0; j < playerCols; j++) {
+			if ((i*playerCols+j) >= numPlayers) {
+				continue;
+			}
+			fill('white');
+			strokeWeight(3)
+			textSize(thumbSize);
+			let pName = players[i*playerCols+j][0];
+			let tWidth = textWidth(pName);
+			if (tWidth > playerWidth) {
+				let tSizeMod = playerWidth/tWidth;
+				textSize(thumbSize*tSizeMod);
+			}
+			if (playerTurn == i*playerCols+j && totalPicks > 0) {
+				fill('red');
+			}
+			textAlign(CENTER, CENTER);
+			text(players[i*playerCols+j][0],j*(playerWidth+thumbSize)+playerWidth/2,i*playerHeight+playerHeight/(playerPickRows+1)/2);
+		}
+	}
+	pop();
+}
+
+function mouseClicked() {
+	if (mouseX < width*0.4 && totalPicks > 0) {
+		let pickRow = Math.floor(mouseY/thumbSize);
+		let pickCol = Math.floor(mouseX/thumbSize);
+		let pick = pickCol+pickRow*11;
+		if (picked[pick] >= 0) {
+			console.log("picked");
+			return 0;
+		}
+		picked[pick] = playerTurn;
+		players[playerTurn][1]++;
+		playerTurn++;
+		playerTurn = playerTurn%numPlayers;
+		totalPicks--;
+	}
+}
